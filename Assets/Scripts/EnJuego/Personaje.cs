be0d0cac2f6cca5a4128a321726cd1c35a212650
DESKTOP-B0DRQC5\admin <ruddy.alpire@gmail.com>
2017-07-28 06:40:41 -0400

    Disparo proyectil

--- Assets/Scripts/EnJuego/Personaje.cs
+++ Assets/Scripts/EnJuego/Personaje.cs
@@ -9,6 +9,15 @@ public class Personaje : MonoBehaviour {
     private int segundosPorAtaque = 4;
     private ConexionFirebase firebase;
 
+    public Transform origenDisparo;
+
+    public Transform destinoBase;
+    public Transform destinoEscudo;
+
+    public Transform destinoPropioEscudo;
+
+    public GameObject proyectil;
+
 	// Use this for initialization
 	void Start () {
         comenzarControl = false;
@@ -26,10 +35,39 @@ public class Personaje : MonoBehaviour {
         {
             if(Time.time - tiempo >= segundosPorAtaque - (0.03f * firebase.DatosPersonaje.Agilidad))
             {
-                float vidaOponente = firebase.DatosPersonajeRemoto.Vida - firebase.DatosPersonaje.Ataque;
                 //firebase.CambiarValor(TipoAtributo.VIDA, vidaOponente);
+                float probabilidadReflejo = 0.2f * firebase.DatosPersonajeRemoto.Defensa;
+                int rand = Random.Range(1, 101);
+                if(probabilidadReflejo >= rand)
+                {
+                    //REFLEJO
+                }
+                else
+                {
+                    //DEFENSA
+                    float def = firebase.DatosPersonaje.Ataque * (0.005f * firebase.DatosPersonajeRemoto.Defensa);
+                    float vidaOponente = firebase.DatosPersonajeRemoto.Vida - (firebase.DatosPersonaje.Ataque - def);
+                }
+
                 tiempo = Time.time;
             }
         }
 	}
+
+    private void DispararConReflejo()
+    {
+        var instancia = Instantiate(proyectil, origenDisparo.position, Quaternion.identity);
+        instancia.GetComponent<Proyectil>().Destino = destinoEscudo.position;
+        instancia.GetComponent<Proyectil>().Destino2 = destinoPropioEscudo.position;
+        instancia.GetComponent<Proyectil>().IniciarMovimiento = true;
+        instancia.GetComponent<Proyectil>().reflejar = true;
+    }
+
+    private void DispararBase()
+    {
+        var instancia = Instantiate(proyectil, origenDisparo.position, Quaternion.identity);
+        instancia.GetComponent<Proyectil>().Destino = destinoBase.position;
+        instancia.GetComponent<Proyectil>().IniciarMovimiento = true;
+        instancia.GetComponent<Proyectil>().reflejar = false;
+    }
 }
---
+++ Assets/Scripts/EnJuego/Proyectil.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Proyectil : MonoBehaviour {
+
+    public bool IniciarMovimiento = false;
+    public bool reflejar = true;
+    public Vector3 Destino;
+    public Vector3 Destino2;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (IniciarMovimiento && transform.position.x != Destino.x && transform.position.y != Destino.y)
+        {
+           transform.position = Vector3.MoveTowards(transform.position, Destino, 50 * Time.deltaTime);
+        }
+        else if (IniciarMovimiento)
+        {
+            if (reflejar)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, Destino2, 50 * Time.deltaTime);
+            }
+            if(reflejar && transform.position.x == Destino2.x && transform.position.y == Destino2.y)
+                Destroy(this.gameObject, 1);
+        }
+	}
+}
